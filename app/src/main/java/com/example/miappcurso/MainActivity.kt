08d7af6d88e8527b51c54b81b057057dc61e5f84
lucopa <lcobpar754@g.educaand.es>
2023-12-29 22:55:21 +0100

    ToolBar añadido con menú. Falta implementar navigation y Detalles

--- app/src/main/java/com/example/miappcurso/MainActivity.kt
+++ app/src/main/java/com/example/miappcurso/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.miappcurso
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.miappcurso.databinding.ActivityMainBinding
-
+import com.example.miappcurso.toolBar.MyToolBar
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        MyToolBar().show(this, "LISTA DE PELÍCULAS", true )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.option_one)
+            Toast.makeText(this, "Detalle Películas", Toast.LENGTH_SHORT).show()
+        if (item.itemId == R.id.option_two)
+            startActivity(Intent(this, LoginActivity::class.java))
+        Toast.makeText(this, "Volviste al Login", Toast.LENGTH_SHORT).show()
+
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
